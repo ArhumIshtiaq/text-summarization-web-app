8d9571f5d2243b57a40c837ed1b8964aac20c6e9
Sara-Mahmood <54724546+Sara-Mahmood@users.noreply.github.com>
2020-11-07 10:27:18 +0500

    built  a basic summarizer with file and url input

---
+++ preprocessing.py
@@ -0,0 +1,29 @@
+import nltk
+import os
+import re
+import math
+import operator
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+
+Stopwords = set(stopwords.words('english'))
+wordlemmatizer = WordNetLemmatizer()
+
+
+def remove_special_characters(text):
+    regex = r'[^a-zA-Z0-9\s]'
+    text = re.sub(regex,'',text)
+    return text
+
+def preprocess(text):    
+    text = remove_special_characters(str(text))
+    text = re.sub(r'\d+', '', text)
+    tokenized_words_with_stopwords = word_tokenize(text)
+    tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
+    tokenized_words = [word for word in tokenized_words if len(word) > 1]
+    tokenized_words = [word.lower() for word in tokenized_words]
+
+    return tokenized_words
+
