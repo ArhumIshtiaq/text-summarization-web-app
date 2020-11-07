89f3af18098be835d61314442cea266e4d1319b1
Arhum <arhumishi@gmail.com>
2020-11-07 13:57:53 +0500

    formatting and some little misc changes

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from nltk.tokenize import sent_tokenize
 from preprocessing import preprocess
-from summarize import summarize 
+from summarize import summarize
 import bs4 as bs
 import urllib.request
 import re
 
+
 def main():
 
     source = input('press 1 for link or 0 for a file:')
-    if source == '1':
-        scraped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence')
+    if int(source):
+        scraped_data = urllib.request.urlopen(
+            'https://en.wikipedia.org/wiki/Artificial_intelligence')
         article = scraped_data.read()
 
-        parsed_article = bs.BeautifulSoup(article,'lxml')
+        parsed_article = bs.BeautifulSoup(article, 'lxml')
 
         paragraphs = parsed_article.find_all('p')
 
@@ -22,9 +24,8 @@ def main():
             text += p.text
     else:
 
-
         file = 'sample.txt'
-        file = open(file , 'r')
+        file = open(file, 'r')
         text = file.read()
 
     tokenized_sentence = sent_tokenize(text)
@@ -35,7 +36,8 @@ def main():
     print("Summary:")
 
     print(summary)
-    outF = open('summary.txt',"w")
+    outF = open('summary.txt', "w")
     outF.write(summary)
+
 
 main()
--- preprocessing.py
+++ preprocessing.py
@@ -14,16 +14,17 @@ wordlemmatizer = WordNetLemmatizer()
 
 def remove_special_characters(text):
     regex = r'[^a-zA-Z0-9\s]'
-    text = re.sub(regex,'',text)
+    text = re.sub(regex, '', text)
     return text
 
-def preprocess(text):    
+
+def preprocess(text):
     text = remove_special_characters(str(text))
     text = re.sub(r'\d+', '', text)
     tokenized_words_with_stopwords = word_tokenize(text)
-    tokenized_words = [word for word in tokenized_words_with_stopwords if word not in Stopwords]
+    tokenized_words = [
+        word for word in tokenized_words_with_stopwords if word not in Stopwords]
     tokenized_words = [word for word in tokenized_words if len(word) > 1]
     tokenized_words = [word.lower() for word in tokenized_words]
 
     return tokenized_words
-
