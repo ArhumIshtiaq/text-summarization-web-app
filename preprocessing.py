2f85c32bcc973faa822949cb45f5f29e233ac500
Arhum <arhumishi@gmail.com>
2020-12-17 01:04:59 +0500

    fixing merge error 2

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,7 @@ import operator
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import heapq
 
 
 Stopwords = set(stopwords.words('english'))
@@ -13,18 +14,46 @@ wordlemmatizer = WordNetLemmatizer()
 
 
 def remove_special_characters(text):
-    regex = r'[^a-zA-Z0-9\s]'
-    text = re.sub(regex, '', text)
-    return text
+    text = re.sub(r'\[[0-9]*\]', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+    sentence_list = nltk.sent_tokenize(article_text)
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    word_frequencies = {}
+    for word in nltk.word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+    maximum_frequncy = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+    summary = ' '.join(summary_sentences)
+    print(summary)
+
+    return summary
 
 
 def preprocess(text):
-    text = remove_special_characters(str(text))
-    text = re.sub(r'\d+', '', text)
-    tokenized_words_with_stopwords = word_tokenize(text)
-    tokenized_words = [
-        word for word in tokenized_words_with_stopwords if word not in Stopwords]
-    tokenized_words = [word for word in tokenized_words if len(word) > 1]
-    tokenized_words = [word.lower() for word in tokenized_words]
+    text = re.sub(r'\[[0-9]*\]', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    formatted_text = re.sub('[^a-zA-Z]', ' ', text )
+    formatted_text = re.sub(r'\s+', ' ', formatted_article_text)
 
-    return tokenized_words
+    return (text, formatted_text)
