4d59defd0b42140bb0b1419a8cd0a983cd34e96d
Sara-Mahmood <54724546+Sara-Mahmood@users.noreply.github.com>
2020-12-16 22:39:02 +0500

    added file upload view

--- textsummarizer/summary/forms.py
+++ textsummarizer/summary/forms.py
@@ -5,3 +5,7 @@ class textForm(forms.Form):
     _type = forms.CharField(label="Choose type of summarization:", widget=forms.Select(choices=[("Abstractive", "Abstractive"), ("Extractive", "Extractive")]))
     text = forms.CharField(label='Enter text to summarize:', widget=forms.Textarea)
     percent = forms.IntegerField(label="Enter summary percentage:", validators=[MinValueValidator(0),MaxValueValidator(100)])
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
+    percent = forms.IntegerField(label="Enter summary percentage:", validators=[MinValueValidator(0),MaxValueValidator(100)])
